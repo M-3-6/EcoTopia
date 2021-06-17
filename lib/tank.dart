7a2e30bdf6030fd46695171ed3c278739acb711a
martho1172001 <martho1172001@gmail.com>
2021-06-17 17:40:27 +0530

    add a tank designed

--- lib/tank.dart
+++ lib/tank.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Tank extends StatefulWidget {
   //const SplashScreen({ Key? key }) : super(key: key);
@@ -24,44 +26,115 @@ class _TankState extends State<Tank> {
           child: Padding(
               padding: EdgeInsets.all(30),
               child: Column(children: [
-                Text("Enter Tank Name:",
-                    style: TextStyle(color: Colors.white, fontSize: 18)),
+                Container(
+                  width: 500,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      "Enter Tank Name:",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
                 TextField(
+                  style: TextStyle(fontSize: 18.0, color: Color(0xFFbdc6cf)),
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Tank 1',
-                      hintStyle: TextStyle(color: Colors.grey)),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Tank 2',
+                    contentPadding: const EdgeInsets.only(
+                        left: 14.0, bottom: 8.0, top: 8.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(25.7),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(25.7),
+                    ),
+                  ),
                 ),
                 SizedBox(height: 20),
-                Text("Enter Tank Capacity:",
-                    style: TextStyle(color: Colors.white, fontSize: 18)),
+                Container(
+                  width: 500,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      "Enter Tank Capacity:",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
                 TextField(
+                  style: TextStyle(fontSize: 18.0, color: Color(0xFFbdc6cf)),
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '500 Ltr',
-                      hintStyle: TextStyle(color: Colors.grey)),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: '500 Ltr',
+                    contentPadding: const EdgeInsets.only(
+                        left: 14.0, bottom: 8.0, top: 8.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(25.7),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(25.7),
+                    ),
+                  ),
                 ),
                 SizedBox(height: 20),
-                Text("Type of Water Supply:",
-                    style: TextStyle(color: Colors.white, fontSize: 18)),
-                DropdownButton<String>(
-                  hint: Text("Panchayat Connection"),
-                  value: dropdownValue,
-                  onChanged: (newValue) {
-                    setState(() {
-                      dropdownValue = newValue;
-                    });
-                  },
-                  items: <String>[
-                    'Panchayat Connection',
-                    'Harvested Rainwater',
-                    'Well Connection'
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
+                Container(
+                  width: 500,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+                    child: Text(
+                      "Type of Water Supply:",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 500,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                    child: DropdownButton<String>(
+                      icon: Padding(
+                        padding: EdgeInsets.only(left: 130),
+                        child: FaIcon(FontAwesomeIcons.arrowDown,
+                            color: Colors.white),
+                      ),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: TextStyle(color: Colors.black),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.grey,
+                      ),
+                      hint: Text("Panchayat Connection",
+                          style:
+                              TextStyle(color: Colors.blueGrey, fontSize: 18)),
+                      value: dropdownValue,
+                      onChanged: (newValue) {
+                        setState(() {
+                          dropdownValue = newValue;
+                        });
+                      },
+                      items: <String>[
+                        'Panchayat Connection',
+                        'Harvested Rainwater',
+                        'Well Connection'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
                 ),
                 SizedBox(height: 50),
                 FlatButton(
